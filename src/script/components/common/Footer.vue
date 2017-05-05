f08a626070e28f6b1daaa0d06f358ac6d2d8e439
wzhhhhh <13161987838@163.com>
2017-05-05 23:47:03 +0800

    footerstart

---
+++ src/script/components/common/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="m-footer">
+    <router-link to="/home">Go to home</router-link>
+    <router-link to="/furniture">Go to furniture</router-link>
+    <router-link to="/search">Go to search</router-link>
+  </footer>
+</template>
+<script>
+export default {
+  name: "",
+  data: function data() {
+    return {
+
+    }
+  }
+}
+</script>
+<style>
+</style>
--- src/script/components/list/Index.vue
+++ src/script/components/list/Index.vue
@@ -4,16 +4,13 @@
     <main>
       <router-view></router-view>
     </main>
-    <footer class="m-footer">
-      <router-link to="/home">Go to home</router-link>
-      <router-link to="/furniture">Go to furniture</router-link>
-      <router-link to="/search">Go to search</router-link>
-    </footer>
+    <Footer />
   </div>
 </template>
 <script>
 import Header from '../common/Header.vue'
+import Footer from '../common/Footer.vue'
 export default {
-  components: { Header }
+  components: { Header, Footer }
 }
 </script>
