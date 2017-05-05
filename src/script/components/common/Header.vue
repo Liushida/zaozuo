abe310104889dd6d15279db9b72d9be944f63fe8
wzhhhhh <13161987838@163.com>
2017-05-05 23:04:39 +0800

    indexheader

---
+++ src/script/components/common/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="m-header">
+    <div class="logo">
+      <img src="/static/images/biz_res_nav_icon.png" alt="">
+    </div>
+    <div class="empty"></div>
+    <div class="search">
+      <img src="/static/images/biz_res_nav_search_normal.png" alt="">
+    </div>
+    <div class="me">
+      <img src="/static/images/biz_res_nav_me_normal.png" alt="">
+    </div>
+    <div class="cart">
+      <img src="/static/images/biz_res_nav_cart_normal.png" alt="">
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "header",
+  data: function data() {
+    return {
+
+    }
+  }
+}
+</script>
--- src/script/components/list/Index.vue
+++ src/script/components/list/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class='m-index'>
-    <header>header</header>
-    <section>
+    <Header/>
+    <main>
       <router-view></router-view>
-    </section>
-    <footer>
+    </main>
+    <footer class="m-footer">
       <router-link to="/home">Go to home</router-link>
       <router-link to="/furniture">Go to furniture</router-link>
       <router-link to="/search">Go to search</router-link>
@@ -12,6 +12,8 @@
   </div>
 </template>
 <script>
+import Header from '../common/Header.vue'
 export default {
+  components: { Header }
 }
 </script>
