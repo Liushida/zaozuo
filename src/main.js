1f3509292073d364ec5f70fbd835f2fbb36cdffa
wzhhhhh <13161987838@163.com>
2017-05-05 20:07:21 +0800

    resetok

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import details from "./script/components/detail/details.vue"
 import router from './script/router'
+import './style/usage/module/list/home.scss'
 Vue.config.productionTip = false
 
 
