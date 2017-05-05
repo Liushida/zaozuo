dee60e53203d5d6749e7236e37ab299b89742bfa
liushida <954485538@qq.com>
2017-05-05 18:31:38 +0800

    l

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import Vue from 'vue'
 import details from "./script/components/detail/details.vue"
 
 Vue.config.productionTip = false
-
+import router from './script/router'
 new Vue({
   el: '#app',
   router,
   data: function(){
       return {
-          
+
       }
   }
   template: '<router-view></router-view>'
