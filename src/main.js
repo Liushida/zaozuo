74b7a9d71cdd0073fbb664a6c7bc961fa58d2e84
wzhhhhh <13161987838@163.com>
2017-05-05 17:54:01 +0800

    routerok

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
-
 import details from "./script/components/detail/details.vue"
-
+import router from './script/router'
 Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  template: '<router-view></router-view>'
+})
