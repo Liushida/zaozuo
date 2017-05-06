9e719f9cbc623cdc7ee26ef1ea1afdb40ca10833
liushida <954485538@qq.com>
2017-05-06 12:04:38 +0800

    lllllll

---
+++ src/script/components/common/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="m-footer">
+    <router-link to="/home" active-class="active">造作新家</router-link>
+    <router-link to="/furniture" active-class="active">家具</router-link>
+    <router-link to="/search" active-class="active">家居</router-link>
+    <router-link to="/furniture" active-class="active">实景</router-link>
+    <router-link to="/furniture" active-class="active">我们</router-link>
+  </footer>
+</template>
+<script>
+export default {
+  name: "",
+  data: function data() {
+    return {
+
+    }
+  }
+}
+</script>
+<style>
+</style>
---
+++ src/script/components/detail/details.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="details">
+
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: function data() {
+      console.log("fuck you");
+    return {
+    }
+  }
+}
+</script>
+<style>
+</style>
