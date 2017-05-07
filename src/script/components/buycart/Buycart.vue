33d9e5493aa30df838a186f33b8cdeaba9f72367
zhanglibao <11454332@qq.com>
2017-05-07 18:20:09 +0800

    购物车静态ok

---
+++ src/script/components/buycart/Buycart.vue
@@ -0,0 +1,81 @@
+<template>	
+	<div class="m-buycart-box">
+		<div class="m-buycart-header">
+			<div class="m-buycart-back"><img src="/static/images/nav_back.png"></div>
+			<div class="m-buycart-title"><span>购物车</span>(<i>1</i>)</div>
+		</div>
+		<div class="m-buycart-list-box">
+			<ul class="m-buycart-list">
+				<li class="m-buycart-list-goods">
+					<div class="goods-header">
+						￥ <span>666</span>
+					</div>
+					<div class="goods-details">
+						<div class="goods-details-img"><img src="/static/images/biz_res_nav_me_pressed.png"></div>
+						<ul class="goods-details-list">
+							<li>随行桌</li>
+							<li><b>颜色:</b><span><i>red</i></span></li>
+							<li><b>数量:</b><span><i>1</i>件</span></li>
+							<li><b>单价:</b><span>￥<i>2599</i></span></li>
+							<li><b>预计发货:</b><span><i>2017-05-05</i></span></li>
+						</ul>
+					</div>
+					<div class="goods-nav">
+						<span><i></i>编辑</span>
+						<span><i></i>删除</span>
+						<span><i></i>选中</span>
+					</div>
+				</li>
+				<li class="m-buycart-list-goods">
+					<div class="goods-header">
+						￥ <span>666</span>
+					</div>
+					<div class="goods-details">
+						<div class="goods-details-img"><img src="/static/images/biz_res_nav_me_pressed.png"></div>
+						<ul class="goods-details-list">
+							<li>随行桌</li>
+							<li><b>颜色:</b><span><i>red</i></span></li>
+							<li><b>数量:</b><span><i>1</i>件</span></li>
+							<li><b>单价:</b><span>￥<i>2599</i></span></li>
+							<li><b>预计发货:</b><span><i>2017-05-05</i></span></li>
+						</ul>
+					</div>
+					<div class="goods-nav">
+						<span><i></i>编辑</span>
+						<span><i></i>删除</span>
+						<span><i></i>选中</span>
+					</div>
+				</li>
+				<li class="m-buycart-list-goods">
+					<div class="goods-header">
+						￥ <span>666</span>
+					</div>
+					<div class="goods-details">
+						<div class="goods-details-img"><img src="/static/images/biz_res_nav_me_pressed.png"></div>
+						<ul class="goods-details-list">
+							<li>随行桌</li>
+							<li><b>颜色:</b><span><i>red</i></span></li>
+							<li><b>数量:</b><span><i>1</i>件</span></li>
+							<li><b>单价:</b><span>￥<i>2599</i></span></li>
+							<li><b>预计发货:</b><span><i>2017-05-05</i></span></li>
+						</ul>
+					</div>
+					<div class="goods-nav">
+						<span><i></i>编辑</span>
+						<span><i></i>删除</span>
+						<span><i></i>选中</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	
+export default {
+
+}
+
+
+</script>
